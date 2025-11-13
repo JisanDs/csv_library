9f841df884abb7dba0c71697682a0211fd644e82
Jisan Js <mdjisan01823jis@gmail.com>
2025-11-14 00:15:18 +0600

    inital commit

---
+++ cfu/__init__.py
@@ -0,0 +1,3 @@
+from .cfu import BasicCsv, ColMan
+
+__all__ = ["BasicCsv", "ColMan"]
--- project_using_cfu/product_man.py
+++ project_using_cfu/product_man.py
@@ -1,5 +1,9 @@
+import sys, os
+sys.path.append(os.path.dirname(os.path.dirname(__file__)))
+
+
 from cfu import ColMan
-import os
+
 
 
 FILE = "products.csv"
